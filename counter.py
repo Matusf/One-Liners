5b608f47ccd06e994f16e973dad64deb4e576c9e
Matus Ferech <matus.ferech@telekom.com>
2017-12-05 19:53:55 +0000

    Add docs to counter and date_diff

--- counter.py
+++ counter.py
@@ -1,1 +1,8 @@
+'''
+Snippet that counts number elements itterable
+
+Args:
+    itterable
+'''
+
 counter = lambda x: dict(__import__('collections').Counter(x))
--- date_difference.py
+++ date_difference.py
@@ -1,1 +1,18 @@
+'''
+Snippet that displays difference (in days) between two given dates
+
+Input:
+    date1 (list): First date in format <year> <month> <day>
+    date2 (list): First date in format <year> <month> <day>
+
+Computes:
+    int: The difference between given dates
+
+Expanded form:
+    print('Difference [days]: {}'.format(abs(
+        (__import__('datetime').date(
+            *[int(i) for i in input('<year> <month> <day>: ').split()]) -
+        __import__('datetime').date(
+            *[int(i) for i in input('<year> <month> <day>: ').split()])).days)))
+'''
 print('Difference [days]: {}'.format(abs((__import__('datetime').date(*[int(i) for i in input('<year> <month> <day>: ').split()]) - __import__('datetime').date(*[int(i) for i in input('<year> <month> <day>: ').split()])).days)))
